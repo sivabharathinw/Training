104d8568a2adadbc1d1ffd5c97d4b7f3d6413d55
sivabharathinw <sivabarathi.m@nativewit.in>
2026-02-16 16:22:08 +0530

    Task3_Map.dart

--- Dart/Task3_Map.dart
+++ Dart/Task3_Map.dart
@@ -11,6 +11,36 @@ print("after adding multiole values $marks");
 //to acces the  values from map use map[key]
 print("The values marks[English] is { $marks[English]}");
 //to access all keys
+  print("All the keys are :${marks.keys}");
+//to access all values
+print("All the values are :${marks.values}");
+//Loop to read all values
+marks.forEach((key, value) => print("$key:$value"));
+//to update values
+marks.update("computerscience",(value)=>80);
+print("After update the marks of computer science :$marks");
+//update multiple values 
+marks.forEach((key,value){
+  marks[key]=value+5;
+
+});
+print("after updating multiple values by adding of 5$marks");
+//update based on condition
+marks.update("English",(value)=>0);
+print("After update the marks of English :$marks");
+
+
+marks.updateAll((key,value){
+if(value==0){
+  return value+25;
+}
+return value;
+});
+print("after done condition based update the english marks will be :$marks");
+
+
+
+}
 
 
 
