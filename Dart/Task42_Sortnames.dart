57e9a2ceb816b0bcfeb11dfa10fe59d087bf3164
sivabharathinw <sivabarathi.m@nativewit.in>
2026-02-18 17:23:26 +0530

    commit

---
+++ Dart/Task42_Sortnames.dart
@@ -0,0 +1,7 @@
+void main(){
+  List<String>names=["zeenath","yamini","ananya","siva","bharathi"];
+  print("Before sorting  the  name list is $names");
+  names.sort();
+  print("After sorting the name list is $names");
+
+}
