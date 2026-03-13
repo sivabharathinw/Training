ff5dff8755ca24a624b5c302447ccc34b9b9574d
sivabharathinw <sivabarathi.m@nativewit.in>
2026-03-13 09:50:27 +0530

    build automation

--- Flutter/Food_Deliver/food_delivery/lib/main.dart
+++ Flutter/Food_Deliver/food_delivery/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import 'lib/firebase_options.dart';
 import '../view/go_routes/routes.dart';
 import 'package:go_router/go_router.dart';
 void main() async {
